158996d4b1ed7af77c1e06b897772df32d16054b
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2024-06-04 13:40:46 +0100

    template using go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.Execute(w, nil)
+	})
+	log.Fatal(http.ListenAndServe("", nil))
+}
